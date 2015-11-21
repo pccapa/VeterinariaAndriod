7209e39e108d3872ce76a79c2b5d8b7a5e557e19
jcrosemberg <jcro2011@gmail.com>
2015-11-21 08:46:33 -0500

    clase servicio

---
+++ MyApplication/app/src/main/java/pe/edu/upc/veterinaryapp/entities/Servicio.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.veterinaryapp.entities;
+
+/**
+ * Created by jcrosemberg on 21/11/2015.
+ */
+public class Servicio {
+    private String nombre;
+    private String tipo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+}
